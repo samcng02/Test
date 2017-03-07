57e5f3d90ececbdf2f5c90f9ff18d8a85713d480
samcng02 <samcng02@gmail.com>
2017-03-07 13:19:45 +0700

    Update TableTool.cs

--- TableTool.cs
+++ TableTool.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Data;
 using System.Collections;
+using System.Diagnostics;
 //
 using System.Linq;
 using System.Threading.Tasks;
@@ -34,38 +35,116 @@ namespace TestTable
             return resultTable;
         }
 
-        public static DataTable LeftJoinTable(DataTable leftTable, DataTable rightTable)
+        public static DataTable JoinTable2(DataTable leftTable, DataTable rightTable, string[] colLeft, string[] colRight)
         {
-            DataTable resultTable = leftTable.Copy();
-            resultTable.Columns.Add("age");
-
-            foreach (DataRow dr in resultTable.Rows)
+            //set col 
+            DataTable resultTable = leftTable.Clone();
+            foreach (var col in rightTable.Columns)
+            {
+                if (!resultTable.Columns.Contains(col.ToString()))
+                    resultTable.Columns.Add(col.ToString());
+            }
+           
+            foreach (DataRow rowLeftTable in leftTable.Rows)
             {
                 foreach (DataRow rowRightTable in rightTable.Rows)
                 {
-                    if (dr["id"] == rowRightTable["id"])
+                    //check condition
+                    bool condition = true;
+                    for (int i = 0; i < colLeft.Length; i++)
                     {
-                        //datarow is reference, so row changed -> table changed
-                        dr["age"] = rowRightTable["age"];
+                        if (rowLeftTable[colLeft[i]] != rowRightTable[colRight[i]])
+                        {
+                            condition = false;
+                            break;
+                        }
+                    }
+
+                    if (condition)
+                    {
+                        var dr = resultTable.NewRow();//bb create new
+                        foreach (var col in resultTable.Columns)
+                        {
+                            string colName = col.ToString();
+                            dr[colName] = leftTable.Columns.Contains(colName) ? rowLeftTable[colName] : rowRightTable[colName];
+                        }
+
+                        resultTable.Rows.Add(dr);
                     }
                 }
             }
             return resultTable;
         }
 
-        public static DataTable RightJoinTable(DataTable leftTable, DataTable rightTable)
+        public static DataTable LeftJoinTable(DataTable leftTable, DataTable rightTable, string[] colLeft, string[] colRight)
+        {
+            DataTable resultTable = leftTable.Copy();
+            foreach (var col in rightTable.Columns)
+            {
+                if (!resultTable.Columns.Contains(col.ToString()))
+                    resultTable.Columns.Add(col.ToString());
+            }
+
+
+            foreach (DataRow dr in resultTable.Rows)
+            {
+                foreach (DataRow rowRightTable in rightTable.Rows)
+                {
+                    //check condition
+                    bool condition = true;
+                    for (int i = 0; i < colLeft.Length; i++)
+                    {
+                        if (dr[colLeft[i]] != rowRightTable[colRight[i]])
+                        {
+                            condition = false;
+                            break;
+                        }
+                    }
+
+                    if (condition)
+                    {
+                        //datarow is reference, so row changed -> table changed
+                        foreach (var col in rightTable.Columns)
+                        {
+                            dr[col.ToString()] = rowRightTable[col.ToString()];
+                        }
+                    }
+                }
+            }
+            return resultTable;
+        }
+
+        public static DataTable RightJoinTable(DataTable leftTable, DataTable rightTable, string[] colLeft, string[] colRight)
         {
             DataTable resultTable = rightTable.Copy();
-            resultTable.Columns.Add("name");
+            foreach (var col in leftTable.Columns)
+            {
+                if (!resultTable.Columns.Contains(col.ToString()))
+                    resultTable.Columns.Add(col.ToString());
+            }
 
             foreach (DataRow dr in resultTable.Rows)
             {
                 foreach (DataRow rowLeftTable in leftTable.Rows)
                 {
-                    if (dr["id"] == rowLeftTable["id"])
+                    //check condition
+                    bool condition = true;
+                    for (int i = 0; i <  colLeft.Length; i++)
+                    {
+                        if (dr[colLeft[i]] != rowLeftTable[colRight[i]])
+                        {
+                            condition = false;
+                            break;
+                        }
+                    }
+
+                    if (condition)
                     {
                         //datarow is reference, so row changed -> table changed
-                        dr["name"] = rowLeftTable["name"];
+                        foreach (var col in leftTable.Columns)
+                        {
+                            dr[col.ToString()] = rowLeftTable[col.ToString()];
+                        }
                     }
                 }
             }
@@ -89,7 +168,7 @@ namespace TestTable
                 DataTable dtDes = dt.Clone();
                 DataRow[] drs = dt.Select(filter);
 
-                foreach(var dr in drs)
+                foreach (var dr in drs)
                 {
                     dtDes.ImportRow(dr);
                 }
@@ -112,9 +191,9 @@ namespace TestTable
             return resultTable;
         }
 
-        public static void Main()
+        //create data test
+        public static DataSet SetDataTest()
         {
-            //----------create data test--------------------
             DataTable table1 = new DataTable();
             table1.Columns.Add("id");
             table1.Columns.Add("name");
@@ -130,16 +209,42 @@ namespace TestTable
             table2.Rows.Add("01", "20");
             table2.Rows.Add("02", "30");
             table2.Rows.Add("04", "50");
-            //--------------------------------------------------
 
+            DataSet ds = new DataSet();
+            ds.Tables.Add(table1);
+            ds.Tables.Add(table2);
+            return ds;
+        }
+
+        public static void Main()
+        {
+            //create data test
+            DataTable table1 = SetDataTest().Tables[0];
+            DataTable table2 = SetDataTest().Tables[1];
             DataTable rs = new DataTable();
-            rs = Merge(table1, table2);
+
+            //timer
+            var s1 = Stopwatch.StartNew();
+
+            //test
+            //rs = Merge(table1, table2);
             //rs = JoinTable(table1, table2);
-            // rs = LeftJoinTable(table1, table2);
-            // rs = RightJoinTable(table1, table2);
+            //rs = JoinTable2(table1, table2,
+            //    new string[] { "id" },
+            //    new string[] { "id" }
+            //    );
+            //rs = LeftJoinTable(table1, table2,
+            //    new string[] { "id" },
+            //    new string[] { "id" }
+            //    );
+            //rs = RightJoinTable(table1, table2,
+            //    new string[] { "id" },
+            //    new string[] { "id" }
+            //    );
             // rs = AddMultiCol();  
             //rs = Filter(rs, "id=01 or id=03");
-
+            s1.Stop();
+            Console.WriteLine(s1.Elapsed.TotalMilliseconds);
             Console.Read();
         }
     }
